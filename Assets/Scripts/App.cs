08798f7c8eb9ddc4397f7276b3e55c568db23976
Qiu Cheng <qiucheng@sightp.com>
2019-08-23 18:02:32 +0800

    v0.0.2 计时状态的添加

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using Unity.UIWidgets.engine;
 using Unity.UIWidgets.material;
 using Unity.UIWidgets.painting;
 using Unity.UIWidgets.ui;
 using Unity.UIWidgets.widgets;
 using System.Diagnostics;
+using Unity.UIWidgets.async;
 
 namespace PomoTimerApp
 {
@@ -29,29 +31,92 @@ namespace PomoTimerApp
         public readonly static TimeSpan DELAY = TimeSpan.FromMilliseconds(100);
 
         private string mTimeText = "25:00";
+        private string mButtonText = "start";
+
+        private Timer mTimer = null;
+
+        private Stopwatch mStopwatch = null;
 
         public override void initState()
         {
             base.initState();
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
 
-            var timer = Window.instance.periodic(DELAY, () =>
+            mStopwatch = new Stopwatch();
+
+            mTimer = Window.instance.periodic(DELAY, () =>
             {
-                UnityEngine.Debug.LogFormat("DELAYED:{0}", stopwatch.Elapsed.TotalSeconds);
-                setState(() =>
+                var minutes = (int)mStopwatch.Elapsed.TotalMinutes;
+
+                if (minutes == 25)
                 {
-                    mTimeText = string.Format("{0}:{1}", 24 - stopwatch.Elapsed.Minutes, 59 - stopwatch.Elapsed.Seconds);
-                });
+                    UnityEngine.Debug.Log("到达 25 分钟");
+                    return;
+                }
+
+                UnityEngine.Debug.LogFormat("DELAYED:{0}", mStopwatch.Elapsed.TotalSeconds);
+
+                mTimeText = string.Format("{0}:{1}", 24 - mStopwatch.Elapsed.Minutes, 59 - mStopwatch.Elapsed.Seconds);
+
+                if (mStopwatch.IsRunning)
+                {
+                    setState(() => { mButtonText = "running"; });
+                }
+                else if((int)mStopwatch.Elapsed.TotalSeconds == 0)
+                {
+                    setState(() => { mTimeText = "25:00"; });
+                }
+                else
+                {
+                    setState(() => { mButtonText = "paused"; });
+                }
             });
+        }
+
+        public override void dispose()
+        {
+            base.dispose();
+
+            mStopwatch.Stop();
+            mTimer.cancel();
         }
 
         public override Widget build(BuildContext context)
         {
-            return new Text(data: mTimeText, style: new TextStyle(
-                color: Colors.white,
-                fontSize: 50
-            ));
+            return
+                new Stack(
+                    children: new List<Widget>()
+                    {
+                        new Align(
+                        alignment: Alignment.center,
+                        child: new Text(data: mTimeText, style: new TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 54,
+                                    fontWeight: FontWeight.bold
+                                ))
+                        ),
+                        new Align(
+                        alignment: Alignment.bottomCenter,
+                        child: new Container(
+                                    margin: EdgeInsets.only(bottom:32),
+                                    child: new GestureDetector(
+                                            child: new RoundedButton(mButtonText),
+                                            onTap: () => {
+                                                if (mStopwatch.IsRunning)
+                                                {
+                                                    mStopwatch.Stop();
+                                                }
+                                                else
+                                                {
+                                                    mStopwatch.Start();
+                                                }
+                                            }
+                                        )
+                                )
+                        )
+                    }
+                );
+                
+                
         }
     }
 }
---
+++ Assets/Scripts/Widgets/RoundedButton.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using Unity.UIWidgets.foundation;
+using Unity.UIWidgets.material;
+using Unity.UIWidgets.painting;
+using Unity.UIWidgets.ui;
+using Unity.UIWidgets.widgets;
+
+namespace PomoTimerApp
+{
+    public class RoundedButton : StatefulWidget
+    {
+        public string Text { get; }
+        public RoundedButton(string text)
+        {
+            Text = text;
+        }
+
+        public override State createState()
+        {
+            return new RoundedButtonState();
+        }
+    }
+
+    class RoundedButtonState : State<RoundedButton>
+    {
+        public override Widget build(BuildContext context)
+        {
+            return new Container(
+                   width: 140,
+                   height: 140,
+                   decoration: new BoxDecoration(
+                            color: Color.fromRGBO(220, 220, 220, 220),
+                            borderRadius: BorderRadius.circular(100f),
+                            boxShadow: new List<BoxShadow>()
+                            {
+                                new BoxShadow(
+                                    color:Color.fromRGBO(220, 220, 220, 220),
+                                    blurRadius: 0f
+                                    )
+                            }
+                       ),
+                   child: new Center(
+                           child: new Text(
+                               data: widget.Text.ToUpper(),
+                               textAlign: TextAlign.center,
+                               style: new TextStyle(
+                                   color: Colors.black,
+                                   fontSize: 24
+                                   )
+                               )
+                           )
+                );
+        }
+    }
+}
